b3fac43cce0599d0476f8e9b1ec50f608698b4de
Levani Papashvili <papashvililevani@gmail.com>
2020-10-11 20:42:35 -0400

    remove sidebar title

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,8 +5,6 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
-
       <div className="sidebar__top">
         <h3>Levani Papashvil's</h3>
         <ExpandMoreIcon />
