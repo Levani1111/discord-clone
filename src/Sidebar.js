19c325090141cd46ab18d2bc8e6b0dbd740b731a
Levani Papashvili <papashvililevani@gmail.com>
2020-10-12 12:47:19 -0400

    add more icons in Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,13 @@ import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
 import SidebarChannel from "./SidebarChannel";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoIcon from "@material-ui/icons/Info";
+import CallIcon from "@material-ui/icons/Call";
+import { Avatar } from "@material-ui/core";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function Sidebar() {
   return (
@@ -24,6 +31,33 @@ function Sidebar() {
           <SidebarChannel />
           <SidebarChannel />
           <SidebarChannel />
+        </div>
+      </div>
+
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon
+          className="sidebar__voiceIcon"
+          fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p> Stream</p>
+        </div>
+        <div className="sidebar__voiceIcons">
+          <InfoIcon />
+          <CallIcon />
+        </div>
+      </div>
+      <div className="sidebar__profile">
+        <Avatar />
+        <div className="sidebar__profileInfo">
+          <h3>Levani Papashvili</h3>
+          <p>#thisIsMyID</p>
+        </div>
+        <div className="sidebar__profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
         </div>
       </div>
     </div>
