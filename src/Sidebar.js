cbf53e99447c2a3b2d32b2285e7249b74a03b84f
Levani Papashvili <papashvililevani@gmail.com>
2020-10-12 11:31:26 -0400

    add icone

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
   return (
@@ -8,6 +9,15 @@ function Sidebar() {
       <div className="sidebar__top">
         <h3>Levani Papashvil's</h3>
         <ExpandMoreIcon />
+      </div>
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar__addChannel" />
+        </div>
       </div>
     </div>
   );
