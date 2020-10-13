678a8691f349035e0cbc24b6f1d97c8fb939ef65
Levani Papashvili <papashvililevani@gmail.com>
2020-10-12 20:46:33 -0400

    working  chat  componenet

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Chat.css";
 import ChatHeader from "./ChatHeader";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 
 function Chat() {
   return (
     <div className="chat">
       <ChatHeader />
+
+      <div className="chat__messages"></div>
+
+      <div className="chat__input">
+        <AddCircleIcon />
+        <form>
+          <input placeholder={`Message #TESTECHANNEL`} />
+        </form>
+      </div>
     </div>
   );
 }
