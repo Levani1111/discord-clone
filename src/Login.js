d54823066d7c465f19ded680f42598f378566788
Levani Papashvili <papashvililevani@gmail.com>
2020-10-14 12:00:34 -0400

    style login

--- src/Login.js
+++ src/Login.js
@@ -7,8 +7,6 @@ function Login() {
 
   return (
     <div className="login">
-      <h2>I am the login</h2>
-
       <div className="login__logo">
         <img
           src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/98/Discord_logo.svg/800px-Discord_logo.svg.png"
