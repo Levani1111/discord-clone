079cf4643b1ff254031e94ba849a8717d631ddf4
Levani Papashvili <papashvililevani@gmail.com>
2020-10-14 16:43:09 -0400

    some updates

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 import Message from "./Message";
 import ChatHeader from "./ChatHeader";
@@ -6,11 +6,19 @@ import AddCircleIcon from "@material-ui/icons/AddCircle";
 import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
 import GifIcon from "@material-ui/icons/Gif";
 import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
+import { selectChannelId, selectChannelName } from "./features/appSlice";
+import { selectUser } from "./features/userSlice";
+import { useSelector } from "react-redux";
 
 function Chat() {
+  const user = useSelector(selectUser);
+  const channelId = useSelector(selectChannelId);
+  const channelName = useSelector(selectChannelName);
+  const [input, setInput] = useState("");
+
   return (
     <div className="chat">
-      <ChatHeader />
+      <ChatHeader channelName={channelName} />
 
       <div className="chat__messages">
         <Message />
@@ -21,8 +29,17 @@ function Chat() {
       <div className="chat__input">
         <AddCircleIcon fontSize="large" />
         <form>
-          <input placeholder={`Message #TESTECHANNEL`} />
-          <button className="chat__inputButton" type="submit">
+          <input
+            value={input}
+            disabled={!channelId}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder={`Message #${channelName}`}
+          />
+          <button
+            disabled={!channelId}
+            className="chat__inputButton"
+            type="submit"
+          >
             Send Message
           </button>
         </form>
